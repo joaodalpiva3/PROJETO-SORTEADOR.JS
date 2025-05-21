b5227957a5da35dbb7a34ab78d33c3c49cecd66c
joaodalpiva3 <joaodalpiva3@gmail.com>
2025-05-21 08:30:36 -0300

    Primeiro repositório

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById("getValue").addEventListener("click", drawNumber);
+
+function drawNumber() {
+    const min = parseInt(document.getElementById("min").value)
+    const max = parseInt(document.getElementById("max").value)
+
+    if (isNaN(min) || isNaN(max) || min >= max) {
+        document.getElementById("resultado").innerText = "Por favor, insira um valores válidos!";
+        return;
+    
+    
+}
+
+const drawResult = Math.floor(Math.random() * (max - min + 1)) + min;
+document.getElementById("resultado").innerText = `Número sorteado: ${drawResult}`;
+
+}
